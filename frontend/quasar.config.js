cfca8d7006752fe8df692f9ae804a93a9e22a8f6
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2024-05-06 20:57:16 +0200

    urls solution

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -44,8 +44,8 @@ module.exports = configure(function (ctx) {
     build: {
       vueRouterMode: "history", // available values: 'hash', 'history'
 
-      // transpile: false,
-      // publicPath: '/',
+      transpile: false,
+      publicPath: '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
