e7d29cf6af740cbd33079eddfc797ca3fcecb008
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2024-05-17 15:39:17 +0200

    feat: Tasks to update tables Reservations , append hours of the day and clean it at the end

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -2,8 +2,12 @@
 
 namespace App\Console;
 
+use App\Models\Design;
+use App\Models\Reservation;
+use App\Models\Restaurant;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +16,70 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $reservations = Reservation::all(); // * Lista con los json de reservas
+    
+            foreach($reservations as $reservation){
+
+                $today = date('Y/m/d');
+                if($reservation->date == $today){
+                    // Storage::disk('public')->append('output.json', $reservation);
+                    $designs = Design::where('restaurant_id',$reservation->restaurant_id)->get();
+                    foreach($designs as $design){
+    
+                        if($design->id == $reservation->design_id){
+    
+                            $tables = $design->tables;
+                            $decoded_tables = json_decode($tables,true);
+                            foreach($decoded_tables as $key => $table){
+
+                                if($reservation->table_number == $decoded_tables[$key]['number']){
+
+                                    $ocupated_hours = $decoded_tables[$key]['ocupated_hours']; // * Array de las horas
+                             
+                                    if(!in_array($reservation->hour,$ocupated_hours)){
+
+                                        array_push($ocupated_hours,$reservation->hour);
+                                        $decoded_tables[$key]['ocupated_hours'] = $ocupated_hours;
+                                    }
+                                }
+                            }
+                            $design->update([
+                                'hall_name' => $design->hall_name,
+                                'tables' =>  $decoded_tables,
+                                'restaurant_id' => $design->restaurant_id
+                            ]);
+                        }
+                    }
+                }
+            }
+        })->everyThirtySeconds();
+
+        $schedule->call(function () {
+
+            $designs = Design::all();
+
+            foreach($designs as $design){
+
+                $tables = $design->tables;
+                $decoded_tables = json_decode($tables,true);
+                foreach($decoded_tables as $key => $table){
+
+                    $decoded_tables[$key]['ocupated_hours'] = [];
+
+                }
+                $design->update([
+                    'hall_name' => $design->hall_name,
+                    'tables' =>  $decoded_tables,
+                    'restaurant_id' => $design->restaurant_id
+                ]);
+
+            }
+
+
+        })->dailyAt('00:00');
+
+
     }
 
     /**
