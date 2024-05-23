b7398bb080ddd5d5c9b6c7ece57c4146c8530ea4
SPiedra955 <samup60@gmail.com>
2024-05-23 02:19:29 +0200

    'updating project funcionality'

--- frontend/src/pages/SendMail.vue
+++ frontend/src/pages/SendMail.vue
@@ -1,42 +1,164 @@
 <template>
-  <q-page class="q-pa-md">
-    <q-form @submit="submitForm">
-      <q-input v-model="name" label="Nombre" required></q-input>
-      <q-input v-model="email" label="Email" type="email" required></q-input>
-      <q-input v-model="message" label="Mensaje" type="textarea" required></q-input>
-      <q-btn label="Enviar" type="submit" color="primary"></q-btn>
-    </q-form>
+  <q-page class="flex flex-center bg-grey-2">
+    <div class="container bg-blue-2 ">
+      <div class="bg-grey-2">
+        <h5 class="text-left text-h5 text-bold text-uppercase mgTop4">Contacto</h5>
+      </div>
+      <form @submit.prevent="sendEmail">
+        <label>Name</label>
+        <input type="text" v-model="name" name="user_name" placeholder="Your Name">
+        <span v-if="errors.name" class="error">{{ errors.name }}</span>
+
+        <label>Email</label>
+        <input type="email" v-model="email" name="user_email" placeholder="Your Email">
+        <span v-if="errors.email" class="error">{{ errors.email }}</span>
+
+        <label>Phone</label>
+        <input type="text" v-model="phone" name="user_phone" placeholder="Your Phone">
+        <span v-if="errors.phone" class="error">{{ errors.phone }}</span>
+
+        <label>Message</label>
+        <textarea name="message" v-model="message" cols="30" rows="5" placeholder="Message"></textarea>
+        <span v-if="errors.message" class="error">{{ errors.message }}</span>
+
+        <input type="submit" value="Send">
+      </form>
+    </div>
   </q-page>
 </template>
 
+
 <script>
+import emailjs from '@emailjs/browser';
+
 export default {
+  name: 'ContactUs',
   data() {
     return {
       name: '',
       email: '',
-      message: ''
+      phone: '',
+      message: '',
+      errors: {
+        name: null,
+        email: null,
+        phone: null,
+        message: null,
+      }
     }
   },
   methods: {
-    async submitForm() {
-      try {
-        await this.$axios.post('http://localhost:8000/api/send-email', {
-          name: this.name,
-          email: this.email,
-          message: this.message
-        });
-        this.$q.notify({
-          type: 'positive',
-          message: 'Correo enviado correctamente!'
-        });
-      } catch (error) {
-        this.$q.notify({
-          type: 'negative',
-          message: 'Error al enviar el correo.'
-        });
+    validateForm() {
+      this.errors = {
+        name: null,
+        email: null,
+        phone: null,
+        message: null,
+      };
+
+      let isValid = true;
+
+      if (!this.name) {
+        this.errors.name = 'Name is required';
+        isValid = false;
       }
-    }
+
+      if (!this.email) {
+        this.errors.email = 'Email is required';
+        isValid = false;
+      } else if (!/\S+@\S+\.\S+/.test(this.email)) {
+        this.errors.email = 'Email is invalid';
+        isValid = false;
+      }
+
+      if (!this.phone) {
+        this.errors.phone = 'Phone number is required';
+        isValid = false;
+      }
+
+      if (!this.message) {
+        this.errors.message = 'Message is required';
+        isValid = false;
+      }
+
+      return isValid;
+    },
+    async sendEmail(e) {
+      if (this.validateForm()) {
+        try {
+          const result = await emailjs.sendForm('service_bd0uxgm', 'template_wrnra98', e.target, 'DZeO-ciVfs13VEYFC');
+          console.log(result.text);
+          alert('Correo enviado con éxito');
+
+          // Limpiar los campos del formulario
+          this.name = '';
+          this.email = '';
+          this.phone = '';
+          this.message = '';
+        } catch (error) {
+          console.error('Error al enviar el correo:', error);
+          alert('Hubo un error al enviar el correo');
+        }
+      } else {
+        alert('Por favor, corrige los errores en el formulario');
+      }
+    },
   }
 }
 </script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  display: block;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  padding: 30px;
+  width: 50%;
+}
+
+label {
+  float: left;
+}
+
+input[type=text],
+[type=email],
+textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.mgTop4 {
+  margin-top: -8vh;
+}
+.error {
+  color: red;
+  font-size: 12px;
+  text-align: left;
+  display: block;
+  margin-bottom: 16px;
+}
+</style>
