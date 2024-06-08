ff045d979227cb97b1555953e234e1b658bd767a
FranN2442 <fgonzalezviejo@cifpfbmoll.eu>
2024-06-08 09:22:12 +0200

    feat | Employees can register users

--- backend/database/seeders/Users/UserSeeder.php
+++ backend/database/seeders/Users/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders\Users;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
